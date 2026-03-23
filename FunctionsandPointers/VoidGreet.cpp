aa402763a66168adfcb9508b6f6c68f6701de5ed
RaghavSeth-13 <guptamona414@gmail.com>
2026-03-23 08:34:03 +0000

    void greet function~

---
+++ FunctionsandPointers/VoidGreet.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using  namespace std ;
+
+void greet(){
+    cout<<"Namaste"<<endl ;
+    cout<<"How are you ? "<<endl;
+return ;
+}
+int main(){
+    greet() ;
+    cout<<"meow meow"<<endl ;
+    return 0 ;
+}
