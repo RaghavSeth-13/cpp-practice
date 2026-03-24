9515dbaf1dcd97ea6e1aa543021f6d8a5ef4907b
RaghavSeth-13 <guptamona414@gmail.com>
2026-03-24 15:33:48 +0000

    LIbraryFunctions.cpp

---
+++ FunctionsandPointers/LIbraryFunctions.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using  namespace std ;
+
+int main(){
+//  cout<<sqrt(7)<<endl ;
+//  cout<<cbrt(675)<<endl ;
+//  cout<<max(45,76)<<endl ;
+//  cout<<min(45,76)<<endl ;
+//  cout<<pow(45,12)<<endl ;
+    return 0 ;
+}
+                    // NOTES
+
+  //sqrt() is a function that calculates the square root of a number.
+  //cbrt() is a function that calculates the cube root of a number.
+// max() is a function that returns the maximum of two numbers.
+// min() is a function that returns the minimum of two numbers.
+// pow() is a function that calculates the power of a number.   
+//max() and min() functions are defined in the <algorithm> header, so you need to include it to use these functions.
