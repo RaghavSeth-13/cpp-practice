9d02096398c2a7f947c764d68d9515a185ed3c1e
RaghavSeth-13 <guptamona414@gmail.com>
2026-03-24 08:31:44 +0000

    MultipleFunctions.cpp

---
+++ FunctionsandPointers/MultipleFunctions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+void russia(){
+    cout<<"kisi ko cylinder nahi milega "<<endl ;
+return ;
+}
+void america(){
+    cout<<"America hi war jeetega or " ;
+    russia() ;
+    return ;
+}
+int main(){
+    america() ;
+    return 0 ;
+}
